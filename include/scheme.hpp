e8ad9219828a2deac290059adbd355a1ca53d4bf
fgwsz <3191000458@qq.com>
2023-06-16 20:41:02 +0800

    wait me

--- include/scheme.hpp
+++ include/scheme.hpp
@@ -1,5 +1,5 @@
 #pragma once
-// 常用概念
+// 一些理念
 
 // 元函数(meta function)
 // 满足如下条件：
@@ -10,8 +10,8 @@
 // 元函数类(meta function class)
 // 为元函数的外覆类，
 // 这么做的目的：
-// 1) 让元函数能作为类型传入其他元函数的参数列表
-// 2) 让元函数调用的结果类型也可以是一个元函数
+// 1) 把元函数包装为类型传入其他元函数的参数列表
+// 2) 元函数调用的结果类型可以是一个元函数类
 // 满足如下条件：
 // 0.本身是一个类型
 // 1.含有成员模板::template apply，表示所代表的元函数
@@ -33,239 +33,79 @@
 // 变量
     // 统一使用蛇形命名法(小写字母写单词，一个下划线作为单词之间的分隔符)
     // 作为模板参数列表的形参时使用一个下划线_作为开头
+// 概念
+    // 统一使用蛇形命名法(小写字母写单词，一个下划线作为单词之间的分隔符)
 
-// 概念的实现
+// 为了区分一个类型通过哪一个类模板实例化而来，
+// 引入标签系统
 
-// 元函数类
+// 标签类
+struct TagOfMetaFunctionClass{};
+struct TagOfStaticConstant{};
+// public继承该类模板的实例可以包含如下语句
+// using tag=_TagType;
+// 非元函数
+template<typename _TagType>
+struct Tag{
+    using tag=_TagType;
+};
+// 元函数类(非元函数)
 template<
     template<typename...>class _MetaFunction
->struct MetaFunctionClass{
+>struct MetaFunctionClass
+    :Tag<TagOfMetaFunctionClass>
+{
     template<typename..._Arguments>
     using apply=_MetaFunction<_Arguments...>;
-    using type=MetaFunctionClass<_MetaFunction>
-        ::template apply<>::type;
 };
-template<
-    template<typename...>class _MetaFunction
->using fn=MetaFunctionClass<_MetaFunction>;
-
-// Bind
-template<
-    typename _MetaFunctionClass,
-    typename..._Arguments
->struct Bind{
-private:
-    template<typename...__Arguments>
-    struct Binder
-    :_MetaFunctionClass::template apply<
-        _Arguments...,
-        __Arguments...
-    >{};
-public:
-    using type=MetaFunctionClass<Binder>;
-};
-// bind_t
-template<
-    typename _MetaFunctionClass,
-    typename..._Arguments
->using Bind_t=typename Bind<
-    _MetaFunctionClass,
-    _Arguments...
->::type;
-// Identity
-template<typename _Type>
-struct Identity{
-    using type=_Type;
-};
-// Constant
+// 编译期常量(非元函数)
 template<
     typename _ValueType,
     _ValueType _value
->
-struct Constant
-:Identity<
-    Constant<
-        _ValueType,
-        _value
-    >
->{
-    using value_type=_ValueType;
-    static constexpr value_type const value=_value;
-};
-//
-template<bool _value>
-using bool_c=Constant<bool,_value>;
-using true_c=Constant<bool,true>;
-using false_c=Constant<bool,true>;
-template<auto _value>
-using auto_c=Constant<decltype(_value),_value>;
-//
-template<
-    typename _ConditionType, 
-    typename _TrueReturnType,
-    typename _FalseReturnType
->struct _IfThenElse;
-template<
-    typename _TrueReturnType,
-    typename _FalseReturnType
->struct _IfThenElse<
-    true_c,
-    _TrueReturnType,
-    _FalseReturnType
->{
-    using type=_TrueReturnType;
-};
-template<
-    typename _TrueReturnType,
-    typename _FalseReturnType
->struct _IfThenElse<
-    false_c,
-    _TrueReturnType,
-    _FalseReturnType
->{
-    using type=_FalseReturnType;
-};
-// _Condition:MetaFunctionClass<>->BooleanConstant
-// _Then:MetaFunction
-template<
-    typename _Condition, 
-    typename _TrueReturn,
-    typename _FalseReturn
->struct IfThenElse{
-    
-};
-template<
-    typename _Constant_1,
-    typename _Constant_2
->struct BinaryOperatorAdd{
-private:
-    static constexpr auto value=
-        _Constant_1::value+
-        _Constant_2::value;
-public:
-    using type=Constant<
-        decltype(value),
-        value
-    >;
-};
-template<
-    typename _Constant_1,
-    typename _Constant_2
->struct BinaryOperatorSub{
-private:
-    static constexpr auto value=
-        _Constant_1::value-
-        _Constant_2::value;
-public:
-    using type=Constant<
-        decltype(value),
-        value
-    >;
-};
-template<
-    typename _Constant_1,
-    typename _Constant_2
->struct BinaryOperatorMul{
-private:
-    static constexpr auto value=
-        _Constant_1::value*
-        _Constant_2::value;
-public:
-    using type=Constant<
-        decltype(value),
-        value
-    >;
-};
-template<
-    typename _Constant_1,
-    typename _Constant_2
->struct BinaryOperatorDiv{
-private:
-    static constexpr auto value=
-        _Constant_1::value/
-        _Constant_2::value;
-public:
-    using type=Constant<
-        decltype(value),
-        value
-    >;
-};
-template<
-    typename _Constant_1,
-    typename _Constant_2
->struct BinaryOperatorMod{
-private:
-    static constexpr auto value=
-        _Constant_1::value%
-        _Constant_2::value;
-public:
-    using type=Constant<
-        decltype(value),
-        value
-    >;
-};
-
-#define __BINARY_OPERATOR(__OPERATOR__,__OPERATOR_NAME__) \
-template< \
-    typename _Constant_1, \
-    typename _Constant_2 \
->struct BinaryOperator_##__OPERATOR_NAME__{ \
-private: \
-    static constexpr auto value= \
-        (_Constant_1::value) __OPERATOR__ \
-        (_Constant_2::value); \
-public: \
-    using type=Constant<
-        decltype(value), \
-        value \
-    >; \
-};
-
-#define __UNARY_OPERATOR(__OPERATOR__,__OPERATOR_NAME__) \
-template<typename _Constant> \
-struct Operator_##__OPERATOR_NAME__{ \
-private: \
-    static constexpr decltype(auto) value= \
-        __OPERATOR__ (_Constant::value); \
-public: \
-    using Type=Auto<value>; \
-};
-
-__META_OPERATOR_BINARY(+,Plus      )
-__META_OPERATOR_BINARY(-,Minus     )
-__META_OPERATOR_BINARY(*,Multiplies)
-__META_OPERATOR_BINARY(/,Divides   )
-__META_OPERATOR_BINARY(%,Modulus   )
-__META_OPERATOR_UNARY (-,Negate    )
-
-__META_OPERATOR_BINARY(==,EqualTo     )
-__META_OPERATOR_BINARY(!=,NotEqualTo  )
-__META_OPERATOR_BINARY(> ,Greater     )
-__META_OPERATOR_BINARY(< ,Less        )
-__META_OPERATOR_BINARY(>=,GreaterEqual)
-__META_OPERATOR_BINARY(<=,LessEqual   )
-
-__META_OPERATOR_BINARY(&&,LogicalAnd)
-__META_OPERATOR_BINARY(||,LogicalOr )
-__META_OPERATOR_UNARY (! ,LogicalNot)
-
-__META_OPERATOR_BINARY(&,BitAnd)
-__META_OPERATOR_BINARY(|,BitOr )
-__META_OPERATOR_BINARY(^,BitXor)
-__META_OPERATOR_UNARY (!,BitNot)
-
-__META_OPERATOR_BINARY(<<,LeftShift )
-__META_OPERATOR_BINARY(>>,RightShift)
-
-#undef __META_OPERATOR_UNARY
-#undef __META_OPERATOR_BINARY
-template<
-template<
-    typename _binary_operator, // function
-    typename _argument_1,
-    typename _argument_2,
-    typename..._arguments
->struct _operator;
+>struct StaticConstant
+    :Tag<TagOfStaticConstant>
 {
-    
+    using value_type=_ValueType;
+    static constexpr value_type value=_value;
 };
+// _c后缀代表这是一个StaticConstant的实例化类型
+// 布尔类型常量
+using true_c=StaticConstant<bool,true>;
+using false_c=StaticConstant<bool,true>;
+// 空常量
+using null_c=StaticConstant<decltype(nullptr),nullptr>;
+// 单分支结构 if then else
+// _Condition:StaticConstant<bool,_value>
+// _Then:type
+// _Else:type
+template<
+    typename _Condition,
+    typename _Then,
+    typename _Else
+>struct IfThenElse;
+template<
+    typename _Then,
+    typename _Else
+>struct IfThenElse<true_c,_Then,_Else>{
+    using type=_Then;
+};
+template<
+    typename _Then,
+    typename _Else
+>struct IfThenElse<false_c,_Then,_Else>{
+    using type=_Else;
+};
+// 单分支结构 if then else
+// _Condition:StaticConstant<bool,_value>
+// _Then:type
+// _Else:type
+template<
+    typename _Condition,
+    typename _Then,
+    typename _Else
+>using if_then_else_t=
+    typename IfThenElse<_Condition,_Then,_Else>::type;
+// 多分支结构 switch case default
+
+// 循环结构 while
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,4 @@
-﻿#include"type_name.hpp"
-#include"c_str_view.hpp"
-#include"person.hpp"
-#include<unordered_map>
-#include<any>
-#include<functional>
-using person_name_t=
-nostd::Field<
-    ::nostd::CStrView<"Person">,
-    ::nostd::CStrView<"name">,
-    &Person::name
->;
-struct FieldInfo{
-    ::std::string class_name;
-    ::std::string field_name;
-    ::std::string_view class_type;
-    ::std::string_view field_type;
-    ::std::any field_pointer;
-};
-struct ClassInfo{
-    ::std::string class_name;
-    ::std::string_view class_type;
-    ::std::unordered_map<::std::string,FieldInfo> field_info;
-    ::std::function<::std::any(::std::vector<::std::any>)> ctor;
-    ::std::function<void(::std::any&)> dtor;
-    ::std::function<::std::any(::std::any const&)> clone;
-    ::std::function<void(::std::any&,::std::any const&)> assign;
-    ::std::function<void(::std::any&,::std::any&)> swap;
-    ::std::function<int(::std::any const&,::std::any const&)> compare;
-};
-::std::unordered_map<::std::string,ClassInfo> class_info;
-
+﻿#include "scheme.hpp"
 int main(void){
-    NOSTD_STATIC_REFLECT_TYPE(::nostd::CStrView<"Person">) person;
-    NOSTD_LOG_TYPE_NAME_OF(person);
     return 0;
 }
