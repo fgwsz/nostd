05699eaaeab31133996d4d83dbd5d55376d2dbfe
fgwsz <3191000458@qq.com>
2023-07-18 08:51:17 +0800

    清理过时的构建脚本

--- build.ps1
+++
@@ -1,8 +0,0 @@
-if(Test-Path -Path build){}else{
-    mkdir build
-}
-cd build
-cmake -G"MinGW Makefiles" ..
-cmake --build .   
-cd ..
-./build/nostd.exe
--- rebuild.ps1
+++
@@ -1,9 +0,0 @@
-if(Test-Path -Path build){
-    Remove-Item build -Recurse
-}
-mkdir build
-cd build
-cmake -G"MinGW Makefiles" ..
-cmake --build .   
-cd ..
-./build/nostd.exe
