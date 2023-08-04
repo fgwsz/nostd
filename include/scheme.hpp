93d945df9d3d830587214f5051359072f64bd650
fgwsz <3191000458@qq.com>
2023-08-04 13:43:38 +0800

    clean code

--- include/scheme.hpp
+++ include/scheme.hpp
@@ -3,12 +3,13 @@
 #include<functional>
 #include<vector>
 #include<map>
+#include<variant>
 namespace scheme{
-
-using string_t=::std::string const;
+// 数据类型
+using boolean_t=bool const;
 using number_t=double const;
-struct Data;
-using data_t=Data const;
+using string_t=::std::string const;
+using data_t=::std::variant<boolean_t,number_t,string_t>const;
 
 using function_name_t=string_t;
 using function_data_t=::std::function<data_t(data_t)>const;
@@ -22,6 +23,7 @@ using function_map_t=::std::map<function_name_t,function_t>;
 static function_map_t function_map{};
 
 enum class Type{
+    boolean,
     number,
     string,
     function,
@@ -30,12 +32,10 @@ enum class Type{
 using type_t=Type const;
 
 using object_name_t=string_t;
-using object_type_t=type_t;
 using object_data_t=data_t;
 
 struct Object{
     object_name_t name;
-    object_type_t type;
     object_data_t data;
 };
 using object_t=Object;
@@ -46,12 +46,24 @@ static object_map_t object_map{};
 using list_t=::std::vector<object_t>;
 
 struct Data{
+    type_t     type_value    ;
+    boolean_t  boolean_value ;
     number_t   number_value  ;
     string_t   string_value  ;
     function_t function_value;
     list_t     list_value    ;
 };
 static constexpr void define_function(function_name_t name,function_data_t data){
-
+    if(function_map.count(name)!=0){
+        return;
+    }
+    function_map.emplace(name,data);
+}
+static constexpr void define_object(object_name_t name,object_data_t data){
+    if(object_map.count(name)!=0){
+        return;
+    }
+    object_map.emplace(name,data);
+}
 };
 }
