b79ad598439a02ca3df6141982536ab7d4cfa504
fgwsz <3191000458@qq.com>
2023-08-04 17:08:12 +0800

    clean code

--- include/scheme.hpp
+++ include/scheme.hpp
@@ -3,67 +3,80 @@
 #include<functional>
 #include<vector>
 #include<map>
-#include<variant>
 namespace scheme{
-// 数据类型
-using boolean_t=bool const;
-using number_t=double const;
-using string_t=::std::string const;
-using data_t=::std::variant<boolean_t,number_t,string_t>const;
 
-using function_name_t=string_t;
-using function_data_t=::std::function<data_t(data_t)>const;
-struct Function{
-    function_name_t name;
-    function_data_t data;
-};
-using function_t=Function const;
+struct Data;
+enum class Type;
+struct Null;
 
-using function_map_t=::std::map<function_name_t,function_t>;
-static function_map_t function_map{};
+using data_t    =Data;
+using type_t    =Type;
+
+using null_t    =Null;
+using boolean_t =bool;
+using number_t  =double;
+using string_t  =::std::string;
+using function_t=::std::function<data_t(data_t)>;
+using list_t    =::std::vector<data_t>;
+
+struct Null{};
 
 enum class Type{
+    null,
     boolean,
     number,
     string,
     function,
     list
 };
-using type_t=Type const;
-
-using object_name_t=string_t;
-using object_data_t=data_t;
-
-struct Object{
-    object_name_t name;
-    object_data_t data;
-};
-using object_t=Object;
-
-using object_map_t=::std::map<object_name_t,object_t>;
-static object_map_t object_map{};
-
-using list_t=::std::vector<object_t>;
 
 struct Data{
     type_t     type_value    ;
+    null_t     null_value    ;
     boolean_t  boolean_value ;
     number_t   number_value  ;
     string_t   string_value  ;
     function_t function_value;
     list_t     list_value    ;
+    Data(null_t value={})
+        :null_value(value)
+        ,type_value(type_t::null)
+    {}
+    Data(boolean_t value)
+        :boolean_value(value)
+        ,type_value(type_t::boolean)
+    {}
+    Data(number_t value)
+        :number_value(value)
+        ,type_value(type_t::number)
+    {}
+    Data(string_t value)
+        :string_value(value)
+        ,type_value(type_t::string)
+    {}
+    Data(function_t value)
+        :function_value(value)
+        ,type_value(type_t::function)
+    {}
+    Data(list_t value)
+        :list_value(value)
+        ,type_value(type_t::list)
+    {}
+    Data(Data const&)=default;
+    Data&opertor=(Data const&)=default;
+    auto& as_null    (){ return null_value    ;}
+    auto& as_boolean (){ return boolean_value ;}
+    auto& as_number  (){ return number_value  ;}
+    auto& as_string  (){ return string_value  ;}
+    auto& as_function(){ return function_value;}
+    auto& as_list    (){ return list_value    ;}
 };
-static constexpr void define_function(function_name_t name,function_data_t data){
-    if(function_map.count(name)!=0){
-        return;
+static function_t number_operator_add=[](data_t parameter_list){
+    auto list=parameter_list.list_value;
+    auto ret=number_t{0};
+    for(auto const& element:list){
+        ret+=element.number_value;
     }
-    function_map.emplace(name,data);
-}
-static constexpr void define_object(object_name_t name,object_data_t data){
-    if(object_map.count(name)!=0){
-        return;
-    }
-    object_map.emplace(name,data);
-}
+    return ret;
 };
 }
