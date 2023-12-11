9046b6b45197c3baaddd60486bee8607d79035fb
fgwsz <3191000458@qq.com>
2023-12-11 10:16:59 +0800

    change git_push.ps1

--- git_push.ps1
+++ git_push.ps1
@@ -1,9 +1,15 @@
-git add include/*
-git add src/*
-git add CMakeLists.txt
-git add msvc_build.ps1
-git add mingw_build.ps1
-git add git_push.ps1
+echo "you can input exit to abort git push."
 $commit_info=Read-Host -Prompt "input commit info"
-git commit -m $commit_info
-git push
+if($commit_info -eq "exit"){
+    echo "git push exit!"
+}else{
+    git add include/*
+    git add src/*
+    git add CMakeLists.txt
+    git add msvc_build.ps1
+    git add mingw_build.ps1
+    git add git_push.ps1
+    $commit_info=Read-Host -Prompt "input commit info"
+    git commit -m $commit_info
+    git push
+}
