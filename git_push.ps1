ae7f90b54c5c596109cfba3b1e7ee686b5bc9beb
fgwsz <3191000458@qq.com>
2023-12-11 10:17:27 +0800

    change git_push.ps1

--- git_push.ps1
+++ git_push.ps1
@@ -9,7 +9,6 @@ if($commit_info -eq "exit"){
     git add msvc_build.ps1
     git add mingw_build.ps1
     git add git_push.ps1
-    $commit_info=Read-Host -Prompt "input commit info"
     git commit -m $commit_info
     git push
 }
